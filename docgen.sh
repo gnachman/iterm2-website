92003fac3a5297bd16ae73b66646a82933c39059
George Nachman <georgen@George-Nachmans-MacBook-Air.local>
2011-03-30 20:00:17 -0700

    Update docs with # of clicks for smart select.

--- docgen.sh
+++ docgen.sh
@@ -1,3 +1,3 @@
 ~/Downloads/Markdown_1.0.1/Markdown.pl < _posts/2011-01-08-documentation.md > /tmp/docs.html
-cat docs_prologue.html /tmp/docs.html docs_epilogue.html > docs.html
+cat docs_prologue.html /tmp/docs.html docs_epilogue.html > /tmp/docs.html
 
