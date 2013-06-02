3c9c490e12cbd7e56798ce2b4261c7088226d007
George Nachman <gitspam.xyz@georgester.com>
2013-06-02 01:26:48 -0700

    Run generate downloads script

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 export PATH="$HOME/bin:$HOME/.gems/bin:$PATH"
 export RUBYLIB="$HOME/lib:$RUBYLIB"
 export GEM_HOME=$HOME/.gems
+./generate_downloads.py > _posts/2013-06-01-downloads.md
 jekyll --server
