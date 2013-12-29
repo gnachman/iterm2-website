2642701676a0749c59a829b198768a4ca859b2f8
George Nachman <github.xyz@georgester.com>
2013-12-29 00:20:42 -0800

    Fix up docgen

--- docgen.sh
+++ docgen.sh
@@ -1,5 +1,6 @@
 echo Run markdown
-~/Downloads/Markdown_1.0.1/Markdown.pl < _posts/2011-01-08-documentation.md > /tmp/docs_in.html
+#~/Downloads/Markdown_1.0.1/Markdown.pl < _posts/2011-01-08-documentation.md > /tmp/docs_in.html
+rdiscount < _posts/2011-01-08-documentation.md > /tmp/docs_in.html
 echo Concatenate
 cat docs_prologue.html /tmp/docs_in.html docs_epilogue.html > /tmp/docs.html
 
