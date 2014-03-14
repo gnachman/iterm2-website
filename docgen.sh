58eb3a092b6b0ce4e389befaa8442acae0f91bf7
Andrew <andrew@hexbrain.com>
2014-03-14 15:21:45 +0200

    Initial commit

--- docgen.sh
+++
@@ -1,6 +0,0 @@
-echo Run markdown
-#~/Downloads/Markdown_1.0.1/Markdown.pl < _posts/2011-01-08-documentation.md > /tmp/docs_in.html
-rdiscount < _posts/2011-01-08-documentation.md > /tmp/docs_in.html
-echo Concatenate
-cat docs_prologue.html /tmp/docs_in.html docs_epilogue.html > /tmp/docs.html
-
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/bin:$HOME/.gems/bin:$PATH"
-export RUBYLIB="$HOME/lib:$RUBYLIB"
-export GEM_HOME=$HOME/.gems
-./generate_downloads.py > _posts/2013-06-01-downloads.md
-jekyll serve
