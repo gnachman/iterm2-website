a7dc289023399503a1a09f22b41d0ce35b2aaf8b
George Nachman <github.xyz@georgester.com>
2014-10-26 22:10:02 -0700

    Increase version of incompatible fish shell. Append a newline to the user's rc script before the source command in case his script ends without a newline.

--- source/misc/install_shell_integration.sh
+++ source/misc/install_shell_integration.sh
@@ -26,9 +26,9 @@ then
 fi
 if [ `basename "${SHELL}"` == fish ]
 then
-  echo "Fish support is nascent. You need a version of fish newer than 2.1.0 (so you probably need to build it yourself). Your version is:"
+  echo "Fish support is nascent. You need a version of fish newer than 2.1.1 (so you probably need to build it yourself). Your version is:"
   fish -v
-  echo "FYI, version 2.1.0 has a bug where long lines wrap prematurely."
+  echo "FYI, version 2.1.1 has a bug where long lines wrap prematurely."
 
   URL="http://iterm2.com/misc/fish_startup.in"
   mkdir -p "${HOME}/.config/fish"
@@ -45,7 +45,7 @@ echo "Downloading script from ${URL} and saving it to ${FILENAME}..."
 curl -L "${URL}" > "${FILENAME}" || die "Couldn't download script from ${URL}"
 chmod +x "${FILENAME}"
 echo "Checking if ${SCRIPT} contains iterm2_shell_integration..."
-grep iterm2_shell_integration "${SCRIPT}" > /dev/null 2>&1 || (echo "Appending source command to ${SCRIPT}..."; echo "source ${FILENAME}" >> "${SCRIPT}")
+grep iterm2_shell_integration "${SCRIPT}" > /dev/null 2>&1 || (echo "Appending source command to ${SCRIPT}..."; echo "" >> "${SCRIPT}"; echo "source ${FILENAME}" >> "${SCRIPT}")
 echo "Done."
 echo ""
 echo "The next time you log in, shell integration will be enabled."
