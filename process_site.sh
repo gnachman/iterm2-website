f3decd07a144008163638a34ca2e77fb6f7e9ed7
George Nachman <gitspam.xyz@georgester.com>
2019-10-08 23:17:27 -0700

    Use backup-uploader

--- process_site.sh
+++ process_site.sh
@@ -9,3 +9,4 @@ cp -R source/python-api/_downloads ~/iterm2.com_stage/python-api/_downloads
 cd ~/iterm2.com_stage && tar cvfz all.tgz *
 cd ~/iterm2.com && tar xvfz ~/iterm2.com_stage/all.tgz
 cp ~/iterm2-website/downloads/stable/.htaccess ~/iterm2.com/downloads/stable/.htaccess
+~/backup-uploader.sh
