185173a8fada3cc4dce2b5d24cccbceefd2580fd
George Nachman <gnachman@gmail.com>
2024-05-17 14:20:47 -0700

    Fix hostname

--- update-patrons.sh
+++ update-patrons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp source/patrons.txt bryan.dreamhost.com:iterm2.com/patrons.txt
+scp source/patrons.txt bryan:iterm2.com/patrons.txt
 echo -n 'https://iterm2.com/patrons.txt' | pbcopy
 open 'https://dash.cloudflare.com/fd2981af5d94d04f7535c150ada305bc/iterm2.com/caching/configuration'
